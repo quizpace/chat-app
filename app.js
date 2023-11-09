c93161c9089835c7ac2b04f90d857857ba7171ad
Avi <drazninavi@gmail.com>
2023-11-09 16:44:23 +0200

    a

--- app.js
+++ app.js
@@ -27,7 +27,68 @@ function sendUserInputToServer(userInput) {
     });
 }
 
-// Function to fetch text from the server and update the chat bubble
+// document.getElementById("send-button").addEventListener("click", function () {
+//   const userInput = document.getElementById("user-input").value;
+//   sendUserInputToServer(userInput);
+// });
+
+// document.getElementById("send-button").addEventListener("click", function () {
+//   const userInput = document.getElementById("user-input").value;
+
+//   // Create a new chat bubble for the user's message
+//   addChatBubble("user", new Date().toLocaleString(), userInput);
+
+//   // Clear the user input field
+//   document.getElementById("user-input").value = "";
+
+//   // Send the user's input to the server (if needed)
+//   sendUserInputToServer(userInput);
+// });
+
+// function sendUserInputToServer(userInput) {
+//   // Send a POST request to the server to store the user's input
+//   fetch("https://db-vkyv.onrender.com/chats", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       user: "user", // You can specify the user here
+//       text: userInput,
+//       time: new Date().toISOString(),
+//     }),
+//   })
+//     .then((response) => response.json())
+//     .then(() => {
+//       // Once the POST request is successful, trigger the GET request to update chat-text
+//       getTextFromServer();
+//     })
+//     .catch((error) => {
+//       console.error("Error sending data:", error);
+//     });
+// }
+
+// function addChatBubble(username, time, text) {
+//   const chatContainer = document.getElementById("chat-container");
+
+//   // Create a new chat bubble element
+//   const chatBubble = document.createElement("div");
+//   chatBubble.classList.add("chat-bubble");
+
+//   // Create the HTML structure for the chat bubble
+//   chatBubble.innerHTML = `
+//     <img class="img-container" src="img/bub.png" alt="Italian Trulli">
+//     <div class="chat-text">
+//       ${username} (${time}): ${text}
+//     </div>
+//   `;
+
+//   // Append the chat bubble to the chat container
+//   chatContainer.appendChild(chatBubble);
+// }
+
+// // Rest of your code
+
 function getTextFromServer() {
   fetch("https://db-vkyv.onrender.com/chats")
     .then((response) => response.json())
@@ -38,7 +99,10 @@ function getTextFromServer() {
 
         const chatBubble = document.getElementById("output");
         const username = latestMessage.user;
-        const time = new Date(latestMessage.time).toLocaleString(); // Convert time to a more readable format
+        const messageDate = new Date(latestMessage.time);
+        const hours = messageDate.getHours();
+        const minutes = messageDate.getMinutes();
+        const time = `${hours}:${minutes}`;
 
         // Create a message format with line breaks
         const message = `${username} <br> (${time}): <br> ${latestMessage.text}`;
